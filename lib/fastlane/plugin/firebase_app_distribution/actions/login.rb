9a647fa60afc13237e138a6713cc8c13bcec410d
Manny Jimenez <mannyjimenez@google.com>
2020-06-03 18:59:17 +0000

    Adding in login

--- lib/fastlane/plugin/firebase_app_distribution/actions/login.rb
+++ lib/fastlane/plugin/firebase_app_distribution/actions/login.rb
@@ -1,8 +1,8 @@
 require 'googleauth'
 require 'googleauth/stores/file_token_store'
 require "google/apis/people_v1"
-#require "googleauth"
-#require "googleauth/stores/file_token_store"
+# require "googleauth"
+# require "googleauth/stores/file_token_store"
 require "fileutils"
 module Fastlane
   module Actions
@@ -13,33 +13,23 @@ module Fastlane
     class LoginAction < Action
       def self.run(params)
         # fastlane will take care of reading in the parameter and fetching the environment variable:
-        UI.message "Parameter API Token: #{params[:api_token]}"
-	UI.message "Running"
 
-        #OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
-        #OOB_URI = "urn:ietf:wg:oauth:2.0:oob"
-        client_id = Google::Auth::ClientId.new("563584335869-fgrhgmd47bqnekij5i8b5pr03ho849e6.apps.googleusercontent.com","j9iVZfS8kkCEFUPaAeJV0sAi")
+        client_id = Google::Auth::ClientId.new("563584335869-fgrhgmd47bqnekij5i8b5pr03ho849e6.apps.googleusercontent.com", "j9iVZfS8kkCEFUPaAeJV0sAi")
         scope = 'https://www.googleapis.com/auth/cloud-platform'
-        token_store = Google::Auth::Stores::FileTokenStore.new(:file => '/usr/local/google/home/mannyjimenez/temp/tokens.yaml')
-	authorizer = Google::Auth::UserAuthorizer.new(client_id, scope,token_store)
-	user_id = "user"
-	credentials = authorizer.get_credentials(user_id)
-	if credentials.nil?
-		url = authorizer.get_authorization_url(base_url: "urn:ietf:wg:oauth:2.0:oob" )
-	  	puts "Open #{url} in your browser and enter the resulting code:"
-	  	code = STDIN.gets
-	  	credentials = authorizer.get_and_store_credentials_from_code( user_id: user_id, code: code, base_url: "urn:ietf:wg:oauth:2.0:oob")
-	  	puts credentials
-	  end
-  #	if ENV['FIREBASE_TOKEN']
-  	#	puts ENV['FIREBASE_TOKEN']
-  	#else
-  		#puts "couldn't find" 
-  	#end
- 	#code = STDIN.gets
-        # sh "shellcommand ./path"
-
-        # Actions.lane_context[SharedValues::TEST_AUTH_CUSTOM_VALUE] = "my_val"
+        token_store = nil
+        authorizer = Google::Auth::UserAuthorizer.new(client_id, scope, token_store)
+        url = authorizer.get_authorization_url(base_url: "urn:ietf:wg:oauth:2.0:oob")
+        UI.message("Please open the following address in your browser:") # \n#{url}"
+        UI.message(url.to_s)
+        UI.message(" ")
+        code = UI.input("Enter the resulting code here: ")
+        credentials = authorizer.get_credentials_from_code(code: code, base_url: "urn:ietf:wg:oauth:2.0:oob")
+        token = credentials.refresh_token
+        UI.message("Refresh Token: #{token}")
+        UI.message(" ")
+        UI.message("Set the refresh token as a FIREBASE_TOKEN enviorment variable")
+      rescue
+        UI.message("An error has occured please login again. Ensure that you have copied the code correctly")
       end
 
       #####################################################
@@ -65,8 +55,8 @@ module Fastlane
                                        env_name: "FL_TEST_AUTH_API_TOKEN", # The name of the environment variable
                                        description: "API Token for TestAuthAction", # a short description of this parameter
                                        verify_block: proc do |value|
-                                          UI.user_error!("No API token for TestAuthAction given, pass using `api_token: 'token'`") unless (value and not value.empty?)
-                                          # UI.user_error!("Couldn't find file at path '#{value}'") unless File.exist?(value)
+                                         UI.user_error!("No API token for TestAuthAction given, pass using `api_token: 'token'`") unless value && !value.empty?
+                                         # UI.user_error!("Couldn't find file at path '#{value}'") unless File.exist?(value)
                                        end),
           FastlaneCore::ConfigItem.new(key: :development,
                                        env_name: "FL_TEST_AUTH_DEVELOPMENT",
@@ -80,7 +70,7 @@ module Fastlane
         # Define the shared values you are going to provide
         # Example
         [
-          ['TEST_AUTH_CUSTOM_VALUE', 'A description of what this value contains']
+          # ['TEST_AUTH_CUSTOM_VALUE', 'A description of what this value contains']
         ]
       end
 
