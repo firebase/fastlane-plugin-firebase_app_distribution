80af9adbdf5d80f0f5962f28d143d583b95d74a1
Manny Jimenez <mannyjimenez@google.com>
2020-06-23 16:41:28 +0000

    adding in another test

--- spec/firebase_app_distribution_action_spec.rb
+++ spec/firebase_app_distribution_action_spec.rb
@@ -1,11 +1,17 @@
 describe Fastlane::Actions::FirebaseAppDistributionAction do
   let(:fake_file) { StringIO.new }
   let(:temp_connection) { double("Connection") }
-  let(:fake_response) { double("Response", body: { status: 200 }) }
+  let(:fake_response) { double("Response", body: { status: 200 , projectNumber: 1, appId: 1, contactEmail: "HelloWorld"})}
+  let(:fake_binary) {double("Binary")}
+  #file = instance_double(File, read: 'stubbed read')
+#allow(File).to receive(:open).and_call_original
+#allow(File).to receive(:open).with('file.txt') { |&block| block.call(file) }
 
-  before do
+  before(:each) do
     allow(Faraday).to receive(:new).and_return(temp_connection)
     allow(temp_connection).to receive(:get).and_return(fake_response)
+    allow(fake_binary).to receive(:read).and_return("Hello World")
+    allow(File).to receive(:open).and_return(fake_binary)
     allow(Tempfile).to receive(:new).and_return(fake_file)
     allow(fake_file).to receive(:unlink)
     allow(fake_file).to receive(:path).and_return("/tmp/string")
@@ -26,7 +32,6 @@ describe Fastlane::Actions::FirebaseAppDistributionAction do
   #     Fastlane::Actions::FirebaseAppDistributionAction.run(params)
   #   end
   # end
-
   describe '#upload_status' do
     it 'checks that you get a response' do
       expected_path = "/v1alpha/apps/app_id/upload_status/token"
@@ -34,6 +39,17 @@ describe Fastlane::Actions::FirebaseAppDistributionAction do
       status = Fastlane::Actions::FirebaseAppDistributionAction.upload_status("token", "app_id")
       expect(status).to eq(200)
     end
+  end 
+
+
+  describe '#get_upload_token' do
+    it 'checks the contact email' do 
+      expect(temp_connection).to receive(:get).with("/v1alpha/apps/app_id")
+      upload_token = Fastlane::Actions::FirebaseAppDistributionAction.get_upload_token("app_id","binary_path")
+      binary_hash = Digest::SHA256.hexdigest(File.open(fake_binary).read)
+      expect(upload_token).to eq(CGI.escape("projects/1/apps/1/releases/-/binaries/#{binary_hash}"))
+  end
+
   end
 
   describe "flag helpers" do
