49aca17cc000222d275dedd87ec46989dd90e9da
Alonso Salas Infante <36527385+alonsosalasinfante@users.noreply.github.com>
2020-06-24 11:45:08 -0700

    Upload status (#57)
    * Uploading newest action changes
    
    * Add Gradle-like polling constants
    
    * Change UI Messages and fix indentation
    
    * Change UI Messages and fix indentation II
    
    * Adds global Faraday connection client
    
    * Refactor upload_token, auth token, and connection client
    
    * Adress PR comments, pending custom exceptions
    
    * Adding in some exceptions
    
    * Adds error handling for missing app id
    
    * Cleaned up error handling responding to Tunde's comments
    
    * fixing typo
    
    * Addressing error handling comment
    
    * Adresses PR comments
    
    * responding to PR comments
    
    * Putting upload into its own function
    
    Co-authored-by: Manny Jimenez <mannyjimenez@google.com>
    Co-authored-by: Alonso Salas Infante <alonsosi@google.com>

--- lib/fastlane/plugin/firebase_app_distribution/actions/firebase_app_distribution_action.rb
+++ lib/fastlane/plugin/firebase_app_distribution/actions/firebase_app_distribution_action.rb
@@ -5,6 +5,7 @@ require 'shellwords'
 require 'googleauth'
 require_relative './firebase_app_distribution_login'
 require_relative '../helper/firebase_app_distribution_helper'
+require_relative '../helper/firebase_app_distribution_error_message'
 
 ## TODO: should always use a file underneath? I think so.
 ## How should we document the usage of release notes?
@@ -15,14 +16,54 @@ module Fastlane
       FIREBASECMD_ACTION = "appdistribution:distribute".freeze
       BASE_URL = "https://firebaseappdistribution.googleapis.com"
       PATH = "/v1alpha/apps/"
+      MAX_POLLING_RETRIES = 60
+      POLLING_INTERVAL_S = 2
 
       extend Helper::FirebaseAppDistributionHelper
 
       def self.run(params)
         params.values # to validate all inputs before looking for the ipa/apk
-        validate_app!(params[:app])
+        platform = Actions.lane_context[Actions::SharedValues::PLATFORM_NAME]
+        binary_path = params[:ipa_path] || params[:apk_path]
+
+        if params[:app] # Set app_id if it is specified as a parameter
+          app_id = params[:app]
+        elsif platform == :ios
+          archive_path = Actions.lane_context[SharedValues::XCODEBUILD_ARCHIVE]
+          if archive_path
+            app_id = findout_ios_app_id_from_archive(archive_path)
+          end
+        end
+
+        if app_id.nil?
+          UI.crash!(ErrorMessage::MISSING_APP_ID)
+        end
+        upload(app_id, binary_path)
       ensure
         cleanup_tempfiles
+      end
+
+      def self.connection
+        @connection ||= Faraday.new(url: BASE_URL) do |conn|
+          conn.response(:json, parser_options: { symbolize_names: true })
+          conn.response(:raise_error) # raise_error middleware will run before the json middleware
+          conn.adapter(Faraday.default_adapter)
+        end
+      end
+
+      def self.auth_token
+        @auth_token ||= begin
+          client = Signet::OAuth2::Client.new(
+            token_credential_uri: 'https://oauth2.googleapis.com/token',
+            client_id: FirebaseAppDistributionLoginAction::CLIENT_ID,
+            client_secret: FirebaseAppDistributionLoginAction::CLIENT_SECRET,
+            refresh_token: ENV["FIREBASE_TOKEN"]
+          )
+          client.fetch_access_token!
+          return client.access_token
+        rescue Signet::AuthorizationError
+          UI.crash!(ErrorMessage::REFRESH_TOKEN_ERROR)
+        end
       end
 
       def self.description
@@ -167,43 +208,54 @@ module Fastlane
         true
       end
 
-      def self.get_token
-        client = Signet::OAuth2::Client.new(
-          token_credential_uri: 'https://oauth2.googleapis.com/token',
-          client_id: FirebaseAppDistributionLoginAction::CLIENT_ID,
-          client_secret: FirebaseAppDistributionLoginAction::CLIENT_SECRET,
-          refresh_token: ENV["FIREBASE_TOKEN"]
-        )
-
-        client.fetch_access_token!
-        return client.access_token
-      rescue Signet::AuthorizationError => error
-        UI.crash!("Wrong value for FIREBASE_TOKEN")
-      end
-
-      def self.validate_app!(app_id)
-        token = get_token
-
-        connection = Faraday.new(url: BASE_URL) do |conn|
-          conn.response(:json, parser_options: { symbolize_names: true })
-          conn.response(:raise_error) # raise_error middleware will run before the json middleware
-          conn.adapter(Faraday.default_adapter)
-        end
+      def self.get_upload_token(app_id, binary_path)
+        binary_hash = Digest::SHA256.hexdigest(File.open(binary_path).read)
         begin
           response = connection.get("#{PATH}#{app_id}") do |request|
-            request.headers["Authorization"] = "Bearer " + token
+            request.headers["Authorization"] = "Bearer " + auth_token
           end
-        rescue Faraday::ResourceNotFound => error
-          UI.user_error!("App Distribution could not find your app #{app_id}. Make sure to onboard your app by pressing the \"Get started\" button on the App Distribution page in the Firebase console: https://console.firebase.google.com/project/_/appdistribution")
-        rescue => error
-          UI.crash!("Failed to fetch app information: #{error.message}")
+        rescue Faraday::ResourceNotFound
+          UI.crash!("App Distribution could not find your app #{app_id}. Make sure to onboard your app by pressing the \"Get started\" button on the App Distribution page in the Firebase console: https://console.firebase.google.com/project/_/appdistribution")
         end
-
         contact_email = response.body[:contactEmail]
-
         if contact_email.strip.empty?
-          UI.user_error!("We could not find a contact email for app #{app_id}. Please visit App Distribution within the Firebase Console to set one up.")
+          UI.crash!(ErrorMessage::GET_APP_NO_CONTACT_EMAIL_ERROR)
         end
+        return CGI.escape("projects/#{response.body[:projectNumber]}/apps/#{response.body[:appId]}/releases/-/binaries/#{binary_hash}")
+      end
+
+      def self.upload_binary(app_id, binary_path)
+        connection.post("/app-binary-uploads?app_id=#{app_id}", File.open(binary_path).read) do |request|
+          request.headers["Authorization"] = "Bearer " + auth_token
+        end
+      end
+
+      def self.upload(app_id, binary_path)
+        upload_token = get_upload_token(app_id, binary_path)
+        status = upload_status(upload_token, app_id)
+        if status == "SUCCESS"
+          UI.message("This APK/IPA has been uploaded before. Skipping upload step.")
+        else
+          MAX_POLLING_RETRIES.times do
+            if status == "SUCCESS"
+              UI.message("Uploaded APK/IPA Successfully!")
+              break
+            elsif status == "IN_PROGRESS"
+              sleep(POLLING_INTERVAL_S)
+            else
+              UI.message("Uploading the APK/IPA")
+              upload_binary(app_id, binary_path)
+            end
+            status = upload_status(upload_token, app_id)
+          end
+        end
+      end
+
+      def self.upload_status(app_token, app_id)
+        response = connection.get("#{PATH}#{app_id}/upload_status/#{app_token}") do |request|
+          request.headers["Authorization"] = "Bearer " + auth_token
+        end
+        response.body[:status]
       end
     end
   end
