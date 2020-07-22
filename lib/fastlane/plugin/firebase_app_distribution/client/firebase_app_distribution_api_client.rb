208a28c05ca1062086fcccc88599a5d206da2bf8
Emmanuel Jimenez <32231345+mannyjimenez0810@users.noreply.github.com>
2020-07-22 17:15:09 -0400

    Google service (#80)
    * Adding in google service account auth method
    
    * Adding in tests and seperating the auth client
    
    * changing file name
    
    * Responding to first round of comments
    
    * Making tests clearer and easier to read
    
    * Cleaning up test and making auth order reflect other systems
    
    * Fixing tests
    
    Co-authored-by: Manny Jimenez <mannyjimenez@google.com>

--- lib/fastlane/plugin/firebase_app_distribution/client/firebase_app_distribution_api_client.rb
+++ lib/fastlane/plugin/firebase_app_distribution/client/firebase_app_distribution_api_client.rb
@@ -9,6 +9,10 @@ module Fastlane
       MAX_POLLING_RETRIES = 60
       POLLING_INTERVAL_SECONDS = 2
 
+      def initialize(auth_token)
+        @auth_token = auth_token
+      end
+
       def enable_access(app_id, release_id, emails, group_ids)
         if emails.nil? && group_ids.nil?
           UI.message("No testers passed in. Skipping this step")
@@ -17,7 +21,7 @@ module Fastlane
         begin
         payload = { emails: emails, groupIds: group_ids }
         connection.post(enable_access_url(app_id, release_id), payload.to_json) do |request|
-          request.headers["Authorization"] = "Bearer " + auth_token
+          request.headers["Authorization"] = "Bearer " + @auth_token
         end
       rescue
         UI.user_error!("#{ErrorMessage::INVALID_TESTERS} \nEmails: #{emails} \nGroups: #{group_ids}")
@@ -33,7 +37,7 @@ module Fastlane
         end
         begin
           connection.post(release_notes_create_url(app_id, release_id), payload.to_json) do |request|
-            request.headers["Authorization"] = "Bearer " + auth_token
+            request.headers["Authorization"] = "Bearer " + @auth_token
           end
         rescue Faraday::ResourceNotFound
           UI.crash!("#{ErrorMessage::INVALID_APP_ID}: #{app_id}")
@@ -50,7 +54,7 @@ module Fastlane
 
         begin
           response = connection.get(v1_apps_url(app_id)) do |request|
-            request.headers["Authorization"] = "Bearer " + auth_token
+            request.headers["Authorization"] = "Bearer " + @auth_token
           end
         rescue Faraday::ResourceNotFound
           UI.crash!("#{ErrorMessage::INVALID_APP_ID}: #{app_id}")
@@ -64,7 +68,7 @@ module Fastlane
 
       def upload_binary(app_id, binary_path)
         connection.post(binary_upload_url(app_id), File.open(binary_path).read) do |request|
-          request.headers["Authorization"] = "Bearer " + auth_token
+          request.headers["Authorization"] = "Bearer " + @auth_token
         end
       rescue Faraday::ResourceNotFound
         UI.crash!("#{ErrorMessage::INVALID_APP_ID}: #{app_id}")
@@ -107,7 +111,7 @@ module Fastlane
       def get_upload_status(app_id, app_token)
         begin
           response = connection.get(upload_status_url(app_id, app_token)) do |request|
-            request.headers["Authorization"] = "Bearer " + auth_token
+            request.headers["Authorization"] = "Bearer " + @auth_token
           end
         rescue Faraday::ResourceNotFound
           UI.crash!("#{ErrorMessage::INVALID_APP_ID}: #{app_id}")
@@ -146,21 +150,6 @@ module Fastlane
           conn.response(:json, parser_options: { symbolize_names: true })
           conn.response(:raise_error) # raise_error middleware will run before the json middleware
           conn.adapter(Faraday.default_adapter)
-        end
-      end
-
-      def auth_token
-        @auth_token ||= begin
-          client = Signet::OAuth2::Client.new(
-            token_credential_uri: TOKEN_CREDENTIAL_URI,
-            client_id: Fastlane::Actions::FirebaseAppDistributionLoginAction::CLIENT_ID,
-            client_secret: Fastlane::Actions::FirebaseAppDistributionLoginAction::CLIENT_SECRET,
-            refresh_token: ENV["FIREBASE_TOKEN"]
-          )
-          client.fetch_access_token!
-          return client.access_token
-        rescue Signet::AuthorizationError
-          UI.crash!(ErrorMessage::REFRESH_TOKEN_ERROR)
         end
       end
     end
