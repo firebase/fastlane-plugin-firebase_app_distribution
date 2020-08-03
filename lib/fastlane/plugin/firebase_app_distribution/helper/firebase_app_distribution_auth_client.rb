0381186158cc4970bd262e302821b157c97eda6f
Emmanuel Jimenez <32231345+mannyjimenez0810@users.noreply.github.com>
2020-08-03 15:16:59 -0400

    Adding in auth with firebase-tools (#108)
    * Adding in auth with firebase-tools
    
    * Deleting a method that is no longer used
    
    * Fixing error support
    
    * Condensing methods
    
    * adding in comment about empty rescue
    
    Co-authored-by: Manny Jimenez <mannyjimenez@google.com>

--- lib/fastlane/plugin/firebase_app_distribution/helper/firebase_app_distribution_auth_client.rb
+++ lib/fastlane/plugin/firebase_app_distribution/helper/firebase_app_distribution_auth_client.rb
@@ -9,9 +9,11 @@ module Fastlane
         if !google_service_path.nil? && !google_service_path.empty?
           service_account(google_service_path)
         elsif ENV["FIREBASE_TOKEN"]
-          firebase_token
+          firebase_token(ENV["FIREBASE_TOKEN"])
         elsif ENV["GOOGLE_APPLICATION_CREDENTIALS"]
           service_account(ENV["GOOGLE_APPLICATION_CREDENTIALS"])
+        elsif (refresh_token = refresh_token_from_firebase_tools)
+          firebase_token(refresh_token)
         else
           UI.crash!(ErrorMessage::MISSING_CREDENTIALS)
         end
@@ -19,13 +21,33 @@ module Fastlane
 
       private
 
-      def firebase_token
+      def refresh_token_from_firebase_tools
+        if ENV["XDG_CONFIG_HOME"]
+          config_path = File.expand_path("configstore/firebase-tools.json", ENV["XDG_CONFIG_HOME"])
+        else
+          config_path = File.expand_path(".config/configstore/firebase-tools.json", "~")
+        end
+
+        if File.exist?(config_path)
+          begin
+           refresh_token = JSON.parse(File.read(config_path))['tokens']['refresh_token']
+           unless refresh_token.nil? && refresh_token.empty?
+             refresh_token
+           end
+         # Rescue is empty to return nil instead of an error
+         # when there is an empty "tokens" field in the firebase-tools json
+         rescue NoMethodError
+         end
+        end
+      end
+
+      def firebase_token(refresh_token)
         begin
           client = Signet::OAuth2::Client.new(
             token_credential_uri: TOKEN_CREDENTIAL_URI,
             client_id: Fastlane::Actions::FirebaseAppDistributionLoginAction::CLIENT_ID,
             client_secret: Fastlane::Actions::FirebaseAppDistributionLoginAction::CLIENT_SECRET,
-            refresh_token: ENV["FIREBASE_TOKEN"]
+            refresh_token: refresh_token
           )
         rescue Signet::AuthorizationError
           UI.crash!(ErrorMessage::REFRESH_TOKEN_ERROR)
