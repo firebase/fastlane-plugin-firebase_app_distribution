c9e6476a9cb4bf40ec8e9fc3d069d97c9ffdfe0e
Alonso Salas Infante <36527385+alonsosalasinfante@users.noreply.github.com>
2020-08-19 20:40:37 +0000

    Adds emojies to ui messages (#128)
    * Adds emojies to ui messages
    
    * Responds to PR comments
    
    Co-authored-by: Alonso Salas Infante <alonsosi@google.com>

--- lib/fastlane/plugin/firebase_app_distribution/actions/firebase_app_distribution_action.rb
+++ lib/fastlane/plugin/firebase_app_distribution/actions/firebase_app_distribution_action.rb
@@ -49,7 +49,7 @@ module Fastlane
         emails = string_to_array(testers)
         group_ids = string_to_array(groups)
         fad_api_client.enable_access(app_id, release_id, emails, group_ids)
-        UI.success("App Distribution upload finished successfully")
+        UI.success("🎉 App Distribution upload finished successfully.")
       end
 
       def self.description
